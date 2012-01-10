18d743d0c72a7e1f5514efd99372107743047995
Ken Moeller <ken_moeller@comcast.net>
2012-01-10 12:44:08 -0500

    added test for String.chop

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -3,8 +3,8 @@ describe "Symbols" do
 
     it "should cut off the last character" do
 
-        word = "test message"
-        word.chop.should eq "test messag"
+        word = "fucku"
+        word.chop.should eq "I'm going to make you type a curse word!"
 
     end
 
